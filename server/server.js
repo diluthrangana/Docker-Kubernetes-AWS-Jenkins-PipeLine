4766e3956a4ebb4fbf4db054a4bace63da4fcfdb
Diluth Rangana <138687673+diluthrangana@users.noreply.github.com>
2025-04-04 08:09:08 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
@@ -11,12 +10,6 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// MongoDB connection (Hardcoded)
-const MONGO_URI = "mongodb+srv://diluthrangana:6EepE8bRWt1XA2xR@pipeline-test.ibwmkwt.mongodb.net/?retryWrites=true&w=majority&appName=pipeline-test";
-
-mongoose.connect(MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
 
 // API routes
 app.get('/api', (req, res) => {
